1b401b4bb02a666a55dc3c85d2f2ab754ecfcf6b
qiang.jin <qiangjin613@163.com>
2021-04-07 15:27:44 +0800

    BasicLevel：1071、1076、1081

---
+++ basic-java/Main_1076.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/**
+ * 1076 Wifi密码 (15 分)
+ */
+public class Main_1076 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        in.nextLine(); // 过滤掉 nextXxx() 留下的分隔符（空格、回车等）
+        while (n-- != 0) {
+            String ansStr = in.nextLine();
+            int index = ansStr.indexOf("T");
+            char c = ansStr.charAt(index - 2);
+            System.out.print(c - 'A' + 1);
+        }
+    }
+}
