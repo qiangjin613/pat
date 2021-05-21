2d76726752af0f409145c555489464282e2893c6
qiang.jin <qiangjin613@163.com>
2021-05-21 14:26:57 +0800

    add BasicLevel：1088、1093

--- basic-java/Main_1092_超时.java
+++ basic-java/Main_1092_超时.java
@@ -1,24 +1,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.StreamTokenizer;
 
 /**
  * 1092 最好吃的月饼 (20 分)
  */
 public class Main_1092_超时 {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine().split(" ");
-        int n = Integer.parseInt(input[0]);
-        int m = Integer.parseInt(input[1]);
+        StreamTokenizer st = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
+        st.nextToken();
+        int n = (int) st.nval;
+        st.nextToken();
+        int m = (int) st.nval;
         // 记录每种月饼的销售额
         int[] kind = new int[n];
         int max = 0;
         // 录入数据并找出销售额最多的值
         while (m-- != 0) {
-            input = br.readLine().split(" ");
-            for (int i = 0; i < input.length; i++) {
-                kind[i] += Integer.parseInt(input[i]);
+            for (int i = 0; i < n; i++) {
+                st.nextToken();
+                kind[i] += (int) st.nval;
                 if (max < kind[i]) {
                     max = kind[i];
                 }
