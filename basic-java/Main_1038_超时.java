82aae137deabd04e43ab100dc2ef21fd6dc54ad7
qiangjin613 <qiangjin613@163.com>
2021-05-22 16:21:03 +0800

    add B 1009

--- basic-java/Main_1038_超时.java
+++ basic-java/Main_1038_超时.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.StreamTokenizer;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,7 +9,6 @@ import java.util.Map;
  * 1038 统计同成绩学生 (20 分)
  */
 public class Main_1038_超时 {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         // 忽略学生总数 N 直接读取第二行数据
@@ -60,5 +60,36 @@ public class Main_1038_超时 {
             }
             flag = true;
         }
+    }
+
+    /**
+     * 使用 StreamTokenizer 也超时
+     */
+    public static void main03(String[] args) throws IOException {
+        StreamTokenizer st = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
+        st.nextToken();
+        int n = (int) st.nval;
+        /* stats[i] 代表分数为 i 的学生数有多少个 */
+        int[] stats = new int[101];
+        for (int i = 0; i < n; i++) {
+            st.nextToken();
+            int index = (int) st.nval;
+            stats[index]++;
+        }
+        // 输出结果：
+        boolean flag = false;
+        StringBuilder output = new StringBuilder(300);
+        st.nextToken();
+        int k = (int) st.nval;
+        for (int i = 0; i < k; i++) {
+            st.nextToken();
+            int index = (int) st.nval;
+            if (flag) {
+                output.append(" ");
+            }
+            output.append(stats[index]);
+            flag = true;
+        }
+        System.out.print(output);
     }
 }
