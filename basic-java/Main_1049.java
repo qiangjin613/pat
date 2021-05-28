a30b3c955d6211339ea7d0ab4df87a54820444ef
qiang.jin <qiangjin613@163.com>
2021-05-28 15:49:05 +0800

    add BasicLevel：1049

--- basic-java/Main_1049.java
+++ basic-java/Main_1049.java
@@ -9,7 +9,7 @@ import java.io.StreamTokenizer;
  * 坑点：浮点数在多次运算后是有误差的，把数值转换为整数再进行计算，避免误差
  *      （但在本题中，并没有指出输入的小数是多少为，所以采用网上大家的，同意放大 1000 倍再进行计算）
  */
-public class Main_1049_超时 {
+public class Main_1049 {
     public static void main(String[] args) throws IOException {
         StreamTokenizer st = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
         st.nextToken();
