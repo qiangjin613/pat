ef077b2c3b0588cc47a19d4b4dbdc80bc1e80a0b
qiangjin613 <qiangjin613@163.com>
2021-06-06 19:20:16 +0800

    update level B: 1084

---
+++ basic-java/Main_1084.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/**
+ * 1084 外观数列 (20 分)
+ */
+public class Main_1084 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        StringBuilder d = new StringBuilder().append(in.nextInt());
+        int n = in.nextInt();
+        while (n-- > 1) {
+            StringBuilder temp = new StringBuilder(16);
+            int j;
+            for (int i = 0; i < d.length(); i = j) {
+                for (j = i; j < d.length() && d.charAt(j) == d.charAt(i); j++);
+                temp.append(d.charAt(i)).append(j - i);
+            }
+            d = temp;
+        }
+        System.out.print(d);
+    }
+}
